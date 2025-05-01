b077882c92fd7b4974431b65a46f324d049e3e1e
siddharth <siddharthjuyal066@gmail.com>
2025-05-01 11:33:29 +0530

    committed the project structure

---
+++ back-end/backservice/src/test/java/in/shoprex/backservice/BackserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.shoprex.backservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
